75ef9178d03ba12a7a0c3dbd2268b3f744da0931
IrumiDesu7 <ikalam89@gmail.com>
2022-06-15 23:54:00 +0800

    fix google map link

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ export default (function data() {
     {
       title: 'Sydney Opera House',
       location: 'AUSTRALIA',
-      googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+      googleMapsUrl: 'https://goo.gl/maps/uZoed4niD8P1Ue5Q9',
       startDate: '27 May, 2021',
       endDate: '8 Jun, 2021',
       description:
@@ -27,7 +27,7 @@ export default (function data() {
     {
       title: 'Geirangerfjord',
       location: 'NORWAY',
-      googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+      googleMapsUrl: 'https://goo.gl/maps/HCoePNz464TQUML39',
       startDate: '01 Oct, 2021',
       endDate: '18 Nov, 2021',
       description:
