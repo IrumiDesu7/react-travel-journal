15a90ed146d449d671d6b388123a086a68ac5f85
IrumiDesu7 <ikalam89@gmail.com>
2022-06-15 23:49:34 +0800

    add finishing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import dataArray from './data';
 
 function App() {
+  const listsData = dataArray.map((data, index) => {
+    return <Main key={index} data={data} />;
+  });
   return (
     <div className='container'>
       <Navbar />
-      <Main />
+      <main>{listsData}</main>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,25 @@
 import locationIcon from '../images/location-icon.png';
 
-export default function Main() {
+export default function Main(props) {
+  console.log(props.data);
   return (
     <div className='lists'>
-      <img
-        src='https://source.unsplash.com/WLxQvbMyfas'
-        alt=''
-        className='photos'
-      />
+      <img src={props.data.imageUrl} alt='' className='photos' />
       <div className='details'>
         <div className='countryContainer'>
           <img src={locationIcon} alt='' className='location-icon' />
-          <p className='country'>JAPAN</p>
-          <a
-            className='gmaps-link'
-            href='https://goo.gl/maps/1DGM5WrWnATgkSNB8'
-          >
+          <p className='country'>{props.data.location}</p>
+          <a className='gmaps-link' href={props.data.googleMapsUrl}>
             View on Google Maps
           </a>
         </div>
-        <h1 className='location-name'>Mount Fuji</h1>
-        <div className='date'>12 Jan, 2021 - 24 Jan, 2021</div>
-        <p className='location-desc'>
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters.
-          Mount Fuji is the single most popular tourist site in Japan, for both
-          Japanese and foreign tourists.
-        </p>
+        <h1 className='location-name'>{props.data.title}</h1>
+        <div className='date'>
+          {props.data.startDate} - {props.data.endDate}
+        </div>
+        <p className='location-desc'>{props.data.description}</p>
       </div>
+      {props.data.title !== 'Geirangerfjord' && <hr />}
     </div>
   );
 }
